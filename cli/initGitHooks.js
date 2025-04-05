4d235f652f2fc6045be1253504377d30f0956dfc
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2025-04-05 18:02:39 +0300

    new change

---
+++ cli/initGitHooks.js
@@ -0,0 +1,26 @@
+const GitHook = require('../src/GitHooks/GitHook');
+const WasabyCLICommand = require('../src/Utils/WasabyCLIComand');
+const { Option } = require('commander');
+
+const options = [
+   new Option(
+      '--hookNames <hooksName...>',
+      'Название хуков, которые инициализировать. По умолчанию устанавливается только pre-commit'
+   )
+      .choices(['pre-commit', 'pre-push'])
+      .default(['pre-commit']),
+];
+
+module.exports = new WasabyCLICommand()
+    .name('initGitHooks')
+    .description('Инициализирует локальные гит хуки от wasaby-cli. Доступно только, если wasaby-cli используется в репозитории.')
+    .addOptions(options)
+    .action(async(options, project) => {
+       const repository = project.store.repositories.get(project.name);
+
+       for (const typeHook of options.params.get('hookNames')) {
+          const hook = new GitHook(typeHook, repository);
+
+          await hook.init();
+       }
+    });
