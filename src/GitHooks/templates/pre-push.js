615ff4719c03df6856c0ea7a3144d264caf485e8
Служебный пользователь Инц №52255 <unix-prog_server@unix.tensor.ru@sbis-dev-mon.unix.tensor.ru>
2026-09-18 18:05:17 +0300

    new change

---
+++ src/GitHooks/templates/pre-push.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+const childProcess = require('node:child_process');
+const os = require('node:os');
+
+const halfMemory = Math.floor(os.totalmem() / 2 / 1024 / 1024);
+
+const child = childProcess.fork(require.resolve('wasaby-cli/cli'), [
+   'runPrePushHook',
+   '--report=console'
+], {
+   cwd: '#ROOT_PROJECT#',
+   execArgv: [
+      `--max-old-space-size=${halfMemory}`,
+   ],
+   env: {
+      ...process.env,
+      NODE_OPTIONS: `--max-old-space-size=${halfMemory}`,
+   }
+});
+
+child.on('exit', (code) => {
+   if (code !== 0) {
+      console.log('Command "git push" was rejected. Pre-push hook finished with errors. See console or debug.log to path "./wasaby-cli_artifacts/runPreCommitHook/debug.log"');
+   }
+
+   process.exit(code);
+});
